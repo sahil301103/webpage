3c70d878d99fbee69a0f8900a52eefb1cc15131b
sahil301103 <kumavatsahil3011@gmail.com>
2024-12-30 17:27:43 +0530

    Add files via upload

---
+++ webpage.js
@@ -0,0 +1,42 @@
+// Get elements
+const menuToggle = document.getElementById("menu-toggle");
+const dropdownMenu = document.getElementById("dropdown-menu");
+const quitButton = document.getElementById("quit-button");
+
+// Show menu on clicking the menu button
+menuToggle.addEventListener("click", () => {
+    dropdownMenu.style.display = "flex";
+});
+
+// Hide menu on clicking the quit button
+quitButton.addEventListener("click", () => {
+    dropdownMenu.style.display = "none";
+});
+// Get elements
+const menuToggle = document.getElementById("menu-toggle");
+const dropdownMenu = document.getElementById("dropdown-menu");
+const quitButton = document.getElementById("quit-button");
+
+// Show menu on clicking the menu button
+menuToggle.addEventListener("click", () => {
+    dropdownMenu.style.display = "flex";
+});
+
+// Hide menu on clicking the quit button
+quitButton.addEventListener("click", () => {
+    dropdownMenu.style.display = "none";
+});
+// Get elements
+const menuToggle = document.getElementById("menu-toggle");
+const dropdownMenu = document.getElementById("dropdown-menu");
+const quitButton = document.getElementById("quit-button");
+
+// Show menu on clicking the menu button
+menuToggle.addEventListener("click", () => {
+    dropdownMenu.style.display = "flex";
+});
+
+// Hide menu on clicking the quit button
+quitButton.addEventListener("click", () => {
+    dropdownMenu.style.display = "none";
+});
